759deda26ac5fed3da2c7f70d96634334c8dbf80
mahdiiithg <saternweb@gmail.com>
2024-01-07 04:58:23 -0800

    UPDATE

--- src/db/mongose.js
+++ src/db/mongose.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL || "mongodb://127.0.0.1:27017/task-manager-api");
+mongoose.connect(process.env.MONGO_URL);
 
 
 
